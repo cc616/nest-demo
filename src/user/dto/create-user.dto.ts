785a221502bd4f77af88809e5c32031f8cedef31
Cuicui Li <813659813@qq.com>
2019-12-04 16:59:36 +0800

    feat: update api doc

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiModelProperty({ example: 'account' })
   readonly account: string;
+
+  @ApiModelProperty({ example: 18 })
   readonly age: number;
+
+  @ApiModelProperty({ example: 'ADMIN' })
   readonly role: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,45 @@
-import { Controller, Get, Req, Post, Body, Param, Res, HttpStatus } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiUseTags,
+} from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 
+@ApiUseTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({
+    title: 'Create user',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async create(@Body() createUserDto: CreateUserDto) {
     await this.userService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({
+    title: 'Find all user',
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    title: 'Find one user by user id',
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   findOne(@Param('id') id: string): Promise<User> {
     return this.userService.findOne(id);
   }
